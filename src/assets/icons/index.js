4665c104261c4d40cd21c140beb9179cfdf6ecd8
Pritam <pri10145@gmail.com>
2023-05-11 13:30:31 +0530

    Service icons added

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,14 @@
 import Logo from './Logo.png'
+import ship from './ship.png'
+import Truck from './Truck.png'
+import warehouse from './warehouse.png'
+import Flight from './Flight.svg'
+
 
 export {
-    Logo
+    Logo,
+    ship,
+    Truck,
+    warehouse,
+    Flight
 }
